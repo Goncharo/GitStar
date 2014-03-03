07e5b4393cf695051ee22690748d40ce879e9c1f
Dima <dima.goncharov@hotmail.com>
2014-03-03 12:32:39 -0800

    Added lab file

---
+++ Lab1/src/p1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+ * @author Dima
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println ("Hello, world");
+
+    }
+
+}
